d7e369603a1c138898238c5f42f24fb78b3e095d
haridhayal11 <haridhayal@gmail.com>
2020-11-03 19:42:13 +0530

    Add toolchain

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ THREAD="-j6"
 
 export ARCH=arm64
 export SUBARCH=arm64
-export CROSS_COMPILE=" /home/haridhayal/Desktop/Kernel/aarch64-linux-android-4.9/bin/aarch64-linux-android-"
+export CROSS_COMPILE=" /home/hari/m20lte/aarch64-linux-android-4.9/bin/aarch64-linux-android-"
 
 make  O=out $CONFIG
 make  O=out $THREAD
