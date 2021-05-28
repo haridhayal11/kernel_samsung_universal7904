8c5a986a627afc2fa77c44ccc4a1e90b77669625
haridhayal11 <haridhayal@gmail.com>
2021-05-28 06:28:01 +0200

    edit script

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 export ARCH=arm64
-export CROSS_COMPILE=../PLATFORM/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+export CROSS_COMPILE=" /home/haridhayal/m20lte/aarch64-linux-android-4.9/bin/aarch64-linux-android-"
 export ANDROID_MAJOR_VERSION=q
 
-make exynos7885-a40_defconfig
-make -j64
+make exynos7885-m20lte_defconfig
+make -j14
